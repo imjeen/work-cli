628f54a305cae64e1d29eb956094a17372123bd7
imjeen <imjeen@sina.com>
2019-12-30 14:42:10 +0800

    chore: update the packages

--- bin/cli.js
+++ bin/cli.js
@@ -1,34 +1,37 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 // chmod +x cli.js           # Make the file executable
-const path = require('path');
-const program = require('commander');
-const pack = require('../package.json');
+const path = require("path");
+const program = require("commander");
+const pack = require("../package.json");
 // process.argv = ['node', 'yourscript.js', ...]
 const [, , ...args] = process.argv;
 // console.log(`hello, ${args}`);
-process.env.NODE_PATH = path.resolve(__dirname, '../node_modules/');
+process.env.NODE_PATH = path.resolve(__dirname, "../node_modules/");
 // console.log(`process.env.NODE_PATH: => ${process.env.NODE_PATH}`);
 
-program.version(pack.version);
+// prettier-ignore
+program
+    .version(pack.version)
+    .description(`work-cli: it is just for work. \nversion: ${pack.version}`);
 
 // init
 program
-    .command('init')
+    .command("init")
+    .alias("i")
     // .option('-f, --foo', 'enable some foo')
-    .description('Generate a new project')
-    .alias('i')
+    .description("Generate a new project")
     .action(() => {
-        require('../commands/init');
+        require("../commands/init");
     });
 // list
 program
-    .command('list')
+    .command("list")
+    .alias("ls")
     // .option('-f, --foo', 'enable some foo')
-    .description('List the templates')
-    .alias('l')
+    .description("List the templates")
     .action(() => {
-        require('../commands/list');
+        require("../commands/list");
     });
 
 // help
